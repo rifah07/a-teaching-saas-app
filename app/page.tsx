bd49e1560d3a85b1a7b493e9d0061dac115ad3f1
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-05-26 14:47:10 +0600

    project folders initialized

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import React from "react";
 
 const Page = () => {
   return (
     <div>
-      <h1 className="text-2xl underline">
-      Welcome to my SaaS App
-      </h1>
+      <h1 className="text-2xl underline">Welcome to my SaaS App</h1>
+      <Button> Get started</Button>
     </div>
-  )
-}
+  );
+};
 
-export default Page
+export default Page;
