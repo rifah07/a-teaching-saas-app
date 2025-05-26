b812e49934ed42335f61466c2cc6c756a8f51c90
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-05-26 17:57:30 +0600

    companion card implemented

--- app/page.tsx
+++ app/page.tsx
@@ -7,9 +7,30 @@ const Page = () => {
     <main>
       <h1>Popular Companions</h1>
       <section className="home-section">
-        <CompanionCard />
-        <CompanionCard />
-        <CompanionCard />
+        <CompanionCard
+          id="123"
+          name="Neura the Brainy Explorer"
+          topic="Neural Network of the Brain"
+          subject="science"
+          duration={45}
+          color="#ffda6e"
+        />
+        <CompanionCard
+          id="456"
+          name="Countsy the Number Wizard"
+          topic="Derivatives and Integrals"
+          subject="maths"
+          duration={30}
+          color="#e5d0ff"
+        />{" "}
+        <CompanionCard
+          id="8910"
+          name="Verba the Vocabolury Builder"
+          topic="language"
+          subject="English Literature"
+          duration={30}
+          color="#BDE7FF"
+        />
       </section>
       <section className="home-section">
         <CompanionsList />
