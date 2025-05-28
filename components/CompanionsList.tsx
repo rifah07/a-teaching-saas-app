61b394c5eb125049d29b5e6cf97146ca678a4a97
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-05-28 17:05:36 +0600

    for lesson and subject row, the table implemented

--- components/CompanionsList.tsx
+++ components/CompanionsList.tsx
@@ -59,7 +59,23 @@ const CompanionsList = ({
                   </div>
                 </Link>
               </TableCell>
-             
+              <TableCell>
+                <div className="subject-badge w-fit max-md:hidden">
+                  {subject}
+                </div>
+                <div
+                  className="flex items-center justify-center rounded-lg  w-fit p-2 md:hidden"
+                  style={{ backgroundColor: getSubjectColor(subject) }}
+                >
+                  <Image
+                    src={`/icons/${subject}.svg`}
+                    alt={subject}
+                    width={18}
+                    height={18}
+                  />
+                </div>
+              </TableCell>
+            
             </TableRow>
           ))}
         </TableBody>
