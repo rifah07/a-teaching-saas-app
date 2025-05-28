2188f8340df33e653bc936d8cdd3ad2a864fcf9b
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-05-28 17:06:32 +0600

    companion list implemented with table

--- components/CompanionsList.tsx
+++ components/CompanionsList.tsx
@@ -75,7 +75,20 @@ const CompanionsList = ({
                   />
                 </div>
               </TableCell>
-            
+              <TableCell>
+                <div className="flex items-center gap-2 w-full justify-end">
+                  <p className="text-2xl">
+                    {duration} <span className="max-md:hidden"> mins</span>
+                  </p>
+                  <Image
+                    src="/icons/clock.svg"
+                    alt="minutes"
+                    width={14}
+                    height={14}
+                    className="md:hidden"
+                  />
+                </div>
+              </TableCell>
             </TableRow>
           ))}
         </TableBody>
