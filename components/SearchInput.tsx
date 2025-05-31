36f3436499d182bc9037fadd67e41360cc79ac11
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-05-31 21:50:46 +0600

    search input fully implemented, Alhamdulillah

--- components/SearchInput.tsx
+++ components/SearchInput.tsx
@@ -1,8 +1,9 @@
 "use client";
 import { usePathname, useSearchParams, useRouter } from "next/navigation";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Image from "next/image";
 import { Input } from "./ui/input";
+import { formUrlQuery, removeKeysFromUrlQuery } from "@jsmastery/utils";
 
 const SearchInput = () => {
   const pathname = usePathname();
@@ -11,6 +12,29 @@ const SearchInput = () => {
   const query = searchParams.get("topic") || "";
 
   const [searchQuery, setSearchQuery] = useState("");
+
+  useEffect(() => {
+    const delayDebounceFn = setTimeout(() => {
+      if (searchQuery) {
+        const newUrl = formUrlQuery({
+          params: searchParams.toString(),
+          key: "topic",
+          value: searchQuery,
+        });
+
+        router.push(newUrl, { scroll: false });
+      } else {
+        if (pathname === "/companions") {
+          const newUrl = removeKeysFromUrlQuery({
+            params: searchParams.toString(),
+            keysToRemove: ["topic"],
+          });
+
+          router.push(newUrl, { scroll: false });
+        }
+      }
+    }, 500);
+  }, [searchQuery, router, searchParams, pathname]);
 
   return (
     <div className="relative border border-black rounded-lg items-center flex gap-2 px-2 py-1 h-fit">
