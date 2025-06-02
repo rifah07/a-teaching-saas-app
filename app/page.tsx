8b1a783e9a0bdacf30794e6baa9852dd94d7b93d
Rifah Sajida Deya <rifahsajida7@gmail.com>
2025-06-03 03:05:07 +0600

    home page updated with real time data

--- app/page.tsx
+++ app/page.tsx
@@ -1,42 +1,32 @@
 import CompanionCard from "@/components/CompanionCard";
 import CompanionsList from "@/components/CompanionsList";
 import Cta from "@/components/Cta"; //Call To Action
-import { recentSessions } from "@/constants";
+//import { recentSessions } from "@/constants";
+import {
+  getAllCompanions,
+  getRecentSessions,
+} from "@/lib/actions/companion.action";
+import { getSubjectColor } from "@/lib/utils";
 
-const Page = () => {
+const Page = async () => {
+  const companions = await getAllCompanions({ limit: 3 });
+  const recentSessionsCompanions = await getRecentSessions(10);
   return (
     <main>
       <h1>Popular Companions</h1>
       <section className="home-section">
-        <CompanionCard
-          id="123"
-          name="Neura the Brainy Explorer"
-          topic="Neural Network of the Brain"
-          subject="science"
-          duration={45}
-          color="#ffda6e"
-        />
-        <CompanionCard
-          id="456"
-          name="Countsy the Number Wizard"
-          topic="Derivatives and Integrals"
-          subject="maths"
-          duration={30}
-          color="#e5d0ff"
-        />{" "}
-        <CompanionCard
-          id="8910"
-          name="Verba the Vocabolury Builder"
-          topic="language"
-          subject="English Literature"
-          duration={30}
-          color="#BDE7FF"
-        />
+        {companions.map((companion) => (
+          <CompanionCard
+            key={companion.id}
+            {...companion}
+            color={getSubjectColor(companion.subject)}
+          />
+        ))}
       </section>
       <section className="home-section">
         <CompanionsList
           title="Recently Completed Sessions"
-          companions={recentSessions}
+          companions={recentSessionsCompanions}
           classNames="w-2/3
           max-lg:w-full"
         />
